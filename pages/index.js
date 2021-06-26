d1d0c639398d221213d1c9fed8c17d4ee35a2ca3
makkarakka3 <49957998+makkarakka3@users.noreply.github.com>
2021-06-26 15:00:50 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-           Art Elokuva OY</a>
+           Art Elokuva OY <a</a>
         </h1>
 
         <p className={styles.description}>
